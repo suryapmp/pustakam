b1756eb0cb038d8e5fd4b0706ffadd3972178dd5
surya <72649807+suryapmp@users.noreply.github.com>
2021-08-27 12:45:42 +0530

    Add files via upload

---
+++ precache-manifest.95670cd5756413d8e87cfc9c97d7c49f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76d348023a048b90cb6c6b3ec40bd68f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d5091284d6659c63f966",
+    "url": "/static/css/main.f2cb2539.chunk.css"
+  },
+  {
+    "revision": "b2021369897ea730686f",
+    "url": "/static/js/2.63d5e88e.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.63d5e88e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5091284d6659c63f966",
+    "url": "/static/js/main.7d99030c.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "c8a48dc5588932b90497415f41e96649",
+    "url": "/static/media/book.c8a48dc5.jpg"
+  },
+  {
+    "revision": "3950ba7ed6ffe0ec421ce857351cefad",
+    "url": "/static/media/bookpic.3950ba7e.jpg"
+  },
+  {
+    "revision": "b53f903149ef8b645ffd1d3e061423f5",
+    "url": "/static/media/books.b53f9031.mp4"
+  }
+]);
